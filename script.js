ee732ee97a24bb6f47e1a86a53d202d96e4aa5e4
luizgabrielmeirazx2-a11y <luizgabrielmeirazx2@gmail.com>
2025-11-18 14:10:13 +0000

    commit

--- script.js
+++
@@ -1,9 +0,0 @@
-// Rolagem suave ao clicar nos links do menu
-document.querySelectorAll('a[href^="#"]').forEach(link => {
-  link.addEventListener('click', function(e) {
-    e.preventDefault();
-    document.querySelector(this.getAttribute('href')).scrollIntoView({
-      behavior: 'smooth'
-    });
-  });
-});
